026404c702b86c39fbdcd57ff6067c9dc6115fff
sangheon <qwer55252@gmail.com>
2026-09-05 00:21:32 +0900

    chore: initialize Talktopia package

---
+++ talktopia/__init__.py
@@ -0,0 +1,3 @@
+"""Talktopia."""
+
+__version__ = "0.1.0"
---
+++ talktopia/pipeline.py
@@ -0,0 +1,28 @@
+
+
+def stage_1_sample_env_profiles():
+    from sotopia.database import EnvironmentProfile
+    from sotopia.database.persistent_profile import EnvironmentList
+
+
+
+def main() -> None:
+    pass
+
+    # Stage 1 : sample
+    env_profiles = stage_1_sample_env_profiles()
+
+
+
+    # Stage 2 : simulate
+
+
+    # Stage 3 : run batch
+
+
+
+    # Stage 4 : evaluate
+
+
+if __name__ == "__main__":
+    main()
